98a4b6dae6e4bb35cef606b36d76a6808172c360
Мария Мельникова <marialnikova@gmail.com>
2023-03-17 20:18:04 +0300

    wf-frontend: added usage icon example

--- wf-frontend/components/Counter/counter.jsx
+++ wf-frontend/components/Counter/counter.jsx
@@ -1,11 +1,18 @@
 import styles from './counter.module.scss'
 
 import {useDispatch, useSelector} from "react-redux";
+import {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
+
 import {add, subtract, clear} from "@/slices/countSlice";
+
 
 export const Counter = () => {
     const count = useSelector(state => state.count.count)
     const dispatch = useDispatch()
+
+    const [heartColor, setHeartColor] = useState(true)
 
     return (
         <div className={styles.counterContainer}>
@@ -15,6 +22,13 @@ export const Counter = () => {
                 <button className={styles.counterButton} onClick={() => dispatch(subtract())}>-</button>
             </div>
             <button className={styles.counterButton} onClick={() => dispatch(clear())}>Clear</button>
+
+            Usage icons example:
+            <FontAwesomeIcon
+                onClick={() => setHeartColor(!heartColor)}
+                cursor="pointer"
+                icon={faHeart} color={heartColor ? 'rgb(255 192 203)' : 'rgb(0 0 0)'}
+            />
         </div>
     )
 }

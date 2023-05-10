cc551a2883d403df7d6659fb88c563341f5f0d44
Maria Melnikova <marialnikova@gmail.com>
2023-05-10 20:12:11 +0300

    Update wd-frontend/pages/account-settings.jsx

--- wd-frontend/pages/account-settings.jsx
+++ wd-frontend/pages/account-settings.jsx
@@ -6,14 +6,13 @@ import styles from './profile.module.scss'
 const SettingsPage = () => {
     return (
       <>
-      
         <Stack>
           <h2>Account</h2>
           <NavLink link="/measurements" icon={<IconLink />} text="Measurements" />
           <NavLink link="/profile" icon={<IconLink />} text="Profile"/>
         </Stack>
         <button className={styles.buttonOut} type="submit">Log out</button>
-        </>
+    </>
       )
   }
   
